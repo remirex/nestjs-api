9c52c27d3eee858108c1e482113734c804314244
Mirko Josimovic <mirkojosimovic1987@gmail.com>
2023-04-07 13:23:59 +0200

    Add print with for prettier config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,18 +2,11 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import {
-  AccessTokenStrategy,
-  RefreshTokenStrategy,
-} from './strategies';
+import { AccessTokenStrategy, RefreshTokenStrategy } from './strategies';
 
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [
-    AuthService,
-    AccessTokenStrategy,
-    RefreshTokenStrategy,
-  ],
+  providers: [AuthService, AccessTokenStrategy, RefreshTokenStrategy],
 })
 export class AuthModule {}
