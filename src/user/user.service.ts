642f50147ddaf1b5d59ebc4ba5dce2659805e0a9
Mirko Josimovic <mirkojosimovic1987@gmail.com>
2023-04-08 15:21:51 +0200

    added promises

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { toUserDto, toUsersDto } from 'src/common/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UserDto } from './dto';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  async getMe(userId: number) {
+  async getMe(userId: number): Promise<UserDto> {
     const user = await this.prisma.user.findUnique({
       where: {
         id: userId,
@@ -16,7 +17,7 @@ export class UserService {
     return toUserDto(user);
   }
 
-  async getUsers() {
+  async getUsers(): Promise<UserDto[]> {
     const users = await this.prisma.user.findMany();
 
     return toUsersDto(users);
