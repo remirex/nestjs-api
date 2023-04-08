6ad68a3abd9b3e4ead8f725b269f2d683127b36a
Mirko Josimovic <mirkojosimovic1987@gmail.com>
2023-04-08 12:58:36 +0200

    OpenApi Swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // OpenAPI
+  const config = new DocumentBuilder()
+    .setTitle('NestJs Api')
+    .setDescription('The Api description')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3333);
 }
 bootstrap();
