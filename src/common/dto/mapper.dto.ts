8593c0fc50831327c1ba6e72ae9634147fed1414
Mirko Josimovic <mirkojosimovic1987@gmail.com>
2023-04-11 22:44:32 +0200

    code refactor

--- src/common/dto/mapper.dto.ts
+++ src/common/dto/mapper.dto.ts
@@ -1,18 +1,9 @@
 import { User } from '@prisma/client';
 import { UserDto } from 'src/user/dto';
+import { exclude } from '../helper';
 
 export const toUserDto = (data: User): UserDto => {
-  const { id, email, role, firstName, lastName, createdAt, updatedAt } = data;
-
-  const userDto: UserDto = {
-    id,
-    email,
-    role,
-    firstName,
-    lastName,
-    createdAt,
-    updatedAt,
-  };
+  const userDto: UserDto = exclude(data, ['hash', 'hashedRt']);
 
   return userDto;
 };
--- src/common/interfaces/index.ts
+++ src/common/interfaces/index.ts
@@ -1,1 +1,2 @@
+export * from './object-literal.interface';
 export * from './page-meta-dto-parameters.interface';
